df77c69e7dcca8eb8959e7ef512e2efa2a9769fc
humbertozanetti <humbertozanetti@users.noreply.github.com>
2025-04-28 14:48:58 -0300

    Create aula01_01.py

---
+++ codigos/aula01_01.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Olá, mundo!')
+st.header('Este é meu primeiro app com Streamlit')
+st.write('Streamlit é uma biblioteca para criar apps web com Python de forma simples.')
